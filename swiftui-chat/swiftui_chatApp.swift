d9a1230a13f76b34e681863a37a5a548d5293cf9
Chris Ching <chris@codewithchris.com>
2021-11-29 14:45:16 -0500

    Initial Commit

---
+++ swiftui-chat/swiftui_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_chatApp.swift
+//  swiftui-chat
+//
+//  Created by Chris Ching on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
