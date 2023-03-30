f5107dc22fcf4ee91ea925978aeaa6a3bd7fbac7
Yean SEYHA <yeanseyha7@gmail.com>
2023-03-30 13:53:03 +0700

    Add font extension

---
+++ swiftui-chat/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  swiftui-chat
+//
+//  Created by Yean Seyha on 30/3/23.
+//
+
+import Foundation
+
+import UIKit
+import Firebase
+
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+                   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
--- swiftui-chat/swiftui_chatApp.swift
+++ swiftui-chat/swiftui_chatApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct swiftui_chatApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             RootView()
         }
     }
+//    init (){
+//        FirebaseApp.configure()
+//    }
 }
