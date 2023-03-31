e031b67244c2b9d6d9424242d35ff2597b8808d5
Yean Seyha <yeanseyha7@gmail.com>
2023-03-31 21:03:22 +0700

    Add Firebase

---
+++ swiftui-chat/ViewModels/AuthViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  AuthViewModel.swift
+//  swiftui-chat
+//
+//  Created by Yean Seyha on 31/3/23.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthViewModel {
+    static func isUserLoggedIn() -> Bool{
+        return Auth.auth().currentUser != nil
+    }
+    
+    static func getLoggedInUserId() -> String{
+        return Auth.auth().currentUser?.uid ?? ""
+    }
+    static func logout(){
+        try? Auth.auth().signOut()
+    }
+}
